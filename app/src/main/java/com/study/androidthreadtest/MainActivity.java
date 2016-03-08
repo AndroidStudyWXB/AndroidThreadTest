efbc54db605b2868ad79be9196076f7e16095a83
WXB506 <1922686887@qq.com>
2016-03-09 07:50:31 +0800

    initial commit

---
+++ app/src/main/java/com/study/androidthreadtest/MainActivity.java
@@ -0,0 +1,40 @@
+package com.study.androidthreadtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    private TextView textView;
+    private Button changeText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.text);
+
+        changeText = (Button) findViewById(R.id.change_text);
+        changeText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.change_text:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText("Nice to meet you");
+                    }
+                }).start();
+                break;
+            default:
+                break;
+        }
+    }
+}
